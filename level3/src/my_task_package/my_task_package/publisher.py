f92ab51af84ec97b2053b8c53b71e4587c273e93
MUHAMMED RUFAID E <muhammedrufaide@gmail.com>
2026-03-22 18:21:48 +0530

    Added Level 3 ROS2 publisher and subscriber

---
+++ level3/src/my_task_package/my_task_package/publisher.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int32
+import random
+
+class DistancePublisher(Node):
+
+    def __init__(self):
+        super().__init__('distance_publisher')
+        self.publisher_ = self.create_publisher(Int32, 'distance', 10)
+        self.timer = self.create_timer(1.0, self.publish_distance)
+
+    def publish_distance(self):
+        msg = Int32()
+        msg.data = random.randint(1, 100)  # random distance
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Publishing distance: {msg.data}')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = DistancePublisher()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ level3/src/my_task_package/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_task_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='renze',
+    maintainer_email='renzekumar034@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'publisher = my_task_package.publisher:main',
+            'subscriber = my_task_package.subscriber:main',
+        ],
+    },
+)
